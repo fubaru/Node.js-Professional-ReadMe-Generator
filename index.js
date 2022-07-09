6e7368f3a33ee46db28dbebb71130eca5e8ba2f5
fuaus <kotingfunk@gmail.com>
2022-07-09 10:32:18 -0400

    add started codes provided in class from Phil

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 // TODO: Include packages needed for this application
-const inquirer = require ('inquirer')
+const inquirer = require ('inquirer');
+const generateMarkdown = require ("./utils/generateMarkdown")
+const fs=require('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your Project Title?"
+
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is your Project description?"
+
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What installaitons are required?"
+
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "choose following license",
+        choices: ["MIT","IBM"]
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions);
+    inquirer.prompt(questions)
+    .then(data=>{
+        console.log(data)
+        fs.writeFileSync("./output/README.md",generateMarkdown(data))
+    })
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -51,7 +51,7 @@ If you followed tutorials, include links to those here as well.
 
 ## License
 
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+${renderLicenseBadge(data.license)}
 
 ---
 
