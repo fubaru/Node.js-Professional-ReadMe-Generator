728703694147305cc4cf168c34d91a7a2b8f335a
fuaus <kotingfunk@gmail.com>
2022-07-12 10:33:22 -0400

    generate an initial README for testing

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,8 +7,14 @@ function renderLicenseBadge(license) {
     return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
   } else if (license==="Apache") {
     return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-  }
-}
+  } else if (license==="Boost") {
+    return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+  } else if (license==="Mozilla") {
+    return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+  } else {
+    return ""
+  };
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
